dca40cca8e75832c0317811e5b5252a0da32b346
Logan Sales <logar56@gmail.com>
2015-03-25 13:16:14 -0500

    adding mmad lab scripts

---
+++ Assets/Classes/ViconVRPN.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class ViconVRPN
+{
+    public static Vector3 vrpnTrackerPos(string host, string obj, int channel)
+    {
+        Vector3 pos = VRPN.vrpnTrackerPos(obj + "@" + host, channel);
+        return new Vector3(pos.x, pos.z, pos.y);
+    }
+	
+    public static Quaternion vrpnTrackerQuat(string host, string obj, int channel)
+    {
+        Quaternion quat = VRPN.vrpnTrackerQuat(obj + "@" + host, channel);
+        return new Quaternion(quat.x, quat.z, quat.y, -quat.w);
+    }
+}
---
+++ Assets/Scripts/TrackerVicon.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackerVicon : MonoBehaviour
+{
+
+    public string host;
+    public string obj;
+    public bool trackPosition;
+    public bool trackRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (trackPosition)
+        {
+            transform.position = ViconVRPN.vrpnTrackerPos(host, obj, 0);
+        }
+        if (trackRotation)
+        {
+            transform.rotation = ViconVRPN.vrpnTrackerQuat(host, obj, 0);
+        }
+    }
+}
